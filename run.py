fb4356b73f0d0eb0f230fb719126d391aee05826
ryan farber <32393930+ryanfarber@users.noreply.github.com>
2020-08-12 22:35:31 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,21 @@
-run.py
+# run.py
+
+from title_maker_pro.word_generator import WordGenerator
+
+word_generator = WordGenerator(
+	device="cpu",
+	forward_model_path="",
+	inverse_model_path="",
+	blacklist_path="",
+	quantize=False,
+	)
+
+
+# word from scratch
+print(word_generator.generate_word())
+
+# definition for a made up word
+print(word_generator.generate_definition("glooberyblipboop"))
+
+# word made up from definition
+print(word_generator.generate_word_from_definition("a word that does not exist"))
